8a42cf72b2e34083331d5528b3b4bafcc490270b
Yifei Sun <ysun@hey.com>
2024-02-24 15:18:36 -0500

    init

---
+++ flake.nix
@@ -0,0 +1,55 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    gomod2nix = {
+      url = "github:nix-community/gomod2nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.flake-utils.follows = "flake-utils";
+    };
+  };
+
+  outputs =
+    { self
+    , nixpkgs
+    , flake-utils
+    , gomod2nix
+    }: flake-utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = import nixpkgs {
+        inherit system;
+        overlays = [
+          gomod2nix.overlays.default
+        ];
+      };
+    in
+    {
+      packages.default = pkgs.buildGoApplication {
+        pname = "churn";
+        version = self.shortRev or self.dirtyShortRev;
+        src = ./.;
+        modules = ./gomod2nix.toml;
+      };
+
+      apps.default = flake-utils.lib.mkApp {
+        drv = self.packages.${system}.default;
+      };
+
+      devShells.default = pkgs.mkShell {
+        packages = with pkgs; [
+          delve
+          go
+          gopls
+          gotools
+          go-tools
+          gomod2nix.packages.${system}.default
+        ];
+      };
+
+      formatter = pkgs.writeShellScriptBin "formatter" ''
+        set -eoux pipefail
+        ${pkgs.nixpkgs-fmt}/bin/nixpkgs-fmt .
+        ${pkgs.go}/bin/gofmt -s -w .
+      '';
+    });
+}
