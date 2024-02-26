a2d1fd58f6293a15d8b7b01289ce824de9e4ae24
Yifei Sun <ysun@hey.com>
2024-02-25 23:13:00 -0500

    connection to godb

--- internal/sqlite/sqlite.go
+++ internal/sqlite/sqlite.go
@@ -6,17 +6,20 @@ import (
 	"path/filepath"
 	"sync"
 
-	_ "github.com/mattn/go-sqlite3"
+	_ "github.com/mattn/go-sqlite3" // SQLite driver
+	"github.com/samonzeweb/godb"
+	adapter "github.com/samonzeweb/godb/adapters/sqlite"
 )
 
 var instance = &sqlite{}
 
 type sqlite struct {
-	conn *sql.DB
-	once sync.Once
+	conn  *sql.DB
+	query *godb.DB
+	once  sync.Once
 }
 
-func Init(db string) (*sql.DB, error) {
+func Init(db string) (*sql.DB, *godb.DB, error) {
 	if _, err := os.Stat(db); os.IsNotExist(err) {
 		os.MkdirAll(filepath.Dir(db), os.ModePerm)
 	}
@@ -24,17 +27,53 @@ func Init(db string) (*sql.DB, error) {
 	var err error
 	instance.once.Do(func() {
 		instance.conn, err = sql.Open("sqlite3", db)
+		if err != nil {
+			return
+		}
+		err = instance.conn.Ping()
+		if err != nil {
+			return
+		}
+
+		instance.query, err = godb.Open(adapter.Adapter, db)
+		if err != nil {
+			return
+		}
+		err = instance.query.CurrentDB().Ping()
+		if err != nil {
+			return
+		}
 	})
-	return instance.conn, err
+
+	return instance.conn, instance.query, nil
 }
 
-func Open() (*sql.DB, error) {
+func Conn() (*sql.DB, error) {
 	if instance.conn == nil {
 		return nil, sql.ErrConnDone
 	}
 	return instance.conn, nil
 }
 
+func Query() (*godb.DB, error) {
+	if instance.query == nil {
+		return nil, sql.ErrConnDone
+	}
+	return instance.query, nil
+}
+
 func Close() error {
-	return instance.conn.Close()
+	var err error
+
+	err = instance.conn.Close()
+	if err != nil {
+		return err
+	}
+
+	err = instance.query.Close()
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
