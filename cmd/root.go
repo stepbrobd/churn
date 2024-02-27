681fe1c86aaeb22bb1f16406374e2d39b24a9615
Yifei Sun <ysun@hey.com>
2024-02-27 11:49:35 -0500

    root cmd use new migration

--- cmd/root.go
+++ cmd/root.go
@@ -2,13 +2,11 @@ package cmd
 
 import (
 	"os"
-	"reflect"
-	"slices"
 
 	"github.com/spf13/cobra"
 	"github.com/stepbrobd/churn/internal/config"
-	"github.com/stepbrobd/churn/internal/sqlite"
-	"github.com/stepbrobd/churn/schema"
+	"github.com/stepbrobd/churn/internal/db"
+	"github.com/stepbrobd/churn/internal/migration"
 )
 
 func preRun(cmd *cobra.Command, args []string) error {
@@ -19,52 +17,21 @@ func preRun(cmd *cobra.Command, args []string) error {
 		}
 	}
 
-	conn, _, err := sqlite.Init(cfg.DatabasePath())
+	err := db.Init(cfg)
 	if err != nil {
 		return err
 	}
 
-	actual := make([]string, 0)
-	expected := map[string]interface{}{
-		"account": &schema.Account{},
-		"bank":    &schema.Bank{},
-		"bonus":   &schema.Bonus{},
-		"product": &schema.Product{},
-		"reward":  &schema.Reward{},
-		"tx":      &schema.Tx{},
-	}
-
-	rows, err := conn.Query("SELECT name FROM sqlite_master WHERE type = 'table'")
+	err = migration.Exec()
 	if err != nil {
 		return err
 	}
-	for rows.Next() {
-		var name string
-		rows.Scan(&name)
-		actual = append(actual, name)
-	}
-
-	tx, err := conn.Begin()
-	if err != nil {
-		return err
-	}
-	for _, t := range reflect.ValueOf(expected).MapKeys() {
-		if !slices.Contains(actual, t.String()) {
-			query := reflect.ValueOf(expected).MapIndex(t).Elem().MethodByName("Schema").Call(nil)[0].String()
-			_, err := tx.Exec(query)
-			if err != nil {
-				tx.Rollback()
-				return err
-			}
-		}
-	}
-	tx.Commit()
 
 	return nil
 }
 
 func postRun(cmd *cobra.Command, args []string) error {
-	return sqlite.Close()
+	return db.Close()
 }
 
 var rootCmd = &cobra.Command{
