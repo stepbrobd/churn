533a823bd9ae9e4a0e3c15116f0682efb06f78e8
Yifei Sun <ysun@hey.com>
2024-04-15 20:13:49 -0400

    text -> varchar

--- internal/migration/0001.sql
+++ internal/migration/0001.sql
@@ -1,19 +1,19 @@
 CREATE TABLE IF NOT EXISTS bank
 (
-    bank_alias TEXT UNIQUE PRIMARY KEY,
-    bank_name TEXT NOT NULL,
+    bank_alias VARCHAR(64) UNIQUE PRIMARY KEY,
+    bank_name VARCHAR(255) NOT NULL,
     max_account INT,
     max_account_period INT,
-    max_account_scope TEXT
+    max_account_scope VARCHAR(64)
 );
 
 CREATE TABLE IF NOT EXISTS product
 (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    product_alias TEXT NOT NULL,
-    product_name TEXT NOT NULL,
+    product_alias VARCHAR(64) UNIQUE NOT NULL,
+    product_name VARCHAR(255) NOT NULL,
     fee DECIMAL NOT NULL,
-    issuing_bank TEXT NOT NULL,
+    issuing_bank VARCHAR(64) NOT NULL,
     FOREIGN KEY (issuing_bank) REFERENCES bank (
         bank_alias
     ) ON DELETE CASCADE ON UPDATE CASCADE
@@ -34,10 +34,10 @@ CREATE TABLE IF NOT EXISTS account
 CREATE TABLE IF NOT EXISTS bonus
 (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    bonus_type TEXT NOT NULL,
+    bonus_type VARCHAR(64) NOT NULL,
     spend DECIMAL NOT NULL,
     bonus_amount DECIMAL NOT NULL,
-    unit TEXT NOT NULL,
+    unit VARCHAR(64) NOT NULL,
     bonus_start DATETIME NOT NULL,
     bonus_end DATETIME NOT NULL,
     account_id INT NOT NULL,
@@ -49,8 +49,8 @@ CREATE TABLE IF NOT EXISTS bonus
 CREATE TABLE IF NOT EXISTS reward
 (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    category TEXT NOT NULL,
-    unit TEXT NOT NULL,
+    category VARCHAR(64) NOT NULL,
+    unit VARCHAR(64) NOT NULL,
     reward DECIMAL NOT NULL,
     product_id INT NOT NULL,
     FOREIGN KEY (product_id) REFERENCES product (
@@ -63,8 +63,8 @@ CREATE TABLE IF NOT EXISTS tx
     id INT AUTO_INCREMENT PRIMARY KEY,
     tx_timestamp DATETIME NOT NULL,
     amount DECIMAL NOT NULL,
-    category TEXT NOT NULL,
-    note TEXT,
+    category VARCHAR(64) NOT NULL,
+    note VARCHAR(255),
     account_id INT NOT NULL,
     FOREIGN KEY (account_id) REFERENCES account (
         id
